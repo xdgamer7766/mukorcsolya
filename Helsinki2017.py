ad99b406929ea8fc3781fbebb516bc6cd0792da3
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2021-01-09 22:49:28 +0100

    feladat3

--- Helsinki2017.py
+++ Helsinki2017.py
@@ -12,3 +12,15 @@ elsoresz = [sor.strip().split(';') for sor in f]
 masodikresz = [sor.strip().split(';') for sor in f0]
 
 print(f"2. Feladat: {len(elsoresz)} versenyző adott elő rövidprogramot")
+
+bejutott = False
+for sor in masodikresz:
+    if sor[1] == "HUN":
+         bejutott = True
+
+if bejutott:
+    print("3.Feladat: Bejutott magyar versenyző a kürtbe")
+else:
+    print("3.Feladat: Nem jutott be magyar versenyző a kürtbe")
+
+
