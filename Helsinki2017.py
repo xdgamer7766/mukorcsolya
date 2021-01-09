c62aec27aa549bc247aba43a72832bdce8a9e44e
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2021-01-09 23:44:26 +0100

    5feladat-6feladat

--- Helsinki2017.py
+++ Helsinki2017.py
@@ -24,3 +24,30 @@ else:
     print("3.Feladat: Nem jutott be magyar versenyző a kürtbe")
 
 
+def OsszPontszam(versenyzo):
+    pontszam = 0.0
+    for sor in elsoresz:
+        if sor[0] == versenyzo:
+            pontszam = pontszam + float(sor[2])
+            pontszam = pontszam + float(sor[3])
+            pontszam = pontszam - float(sor[4])
+    for sor in masodikresz:
+        if sor[0] == versenyzo:
+            pontszam = pontszam + float(sor[2])
+            pontszam = pontszam + float(sor[3])
+            pontszam = pontszam - float(sor[4])
+    return pontszam
+
+
+versenyzo = input("5.Feladat: Kérem a versenyző nevét ")
+resztvett = False
+for sor in elsoresz:
+    if (sor[0] != versenyzo):
+        reszvett = True
+if reszvett:
+    print("Ilyen nevű induló nem volt")
+    print(f"6.Feladat: {OsszPontszam(versenyzo)}")
+
+
+
+
