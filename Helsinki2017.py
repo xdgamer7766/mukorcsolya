d2c51315c4f4cb4a1ac7b0d1bc21c8b15307579c
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2021-01-09 22:40:11 +0100

    feladat2

--- Helsinki2017.py
+++ Helsinki2017.py
@@ -1,5 +1,14 @@
+#Név         ;Ország    ;Technikai   ;Komponens   ;Levonás
+#Zijun LI    ;CHN       ;53.96       ;49.54       ;0
+
+
 f = open("rovidprogram.csv")
 f0 = open("donto.csv")
 
+f.readline()
+f0.readline()
+
 elsoresz = [sor.strip().split(';') for sor in f]
 masodikresz = [sor.strip().split(';') for sor in f0]
+
+print(f"2. Feladat: {len(elsoresz)} versenyző adott elő rövidprogramot")
